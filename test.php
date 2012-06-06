5817b23d46b89c020d73f9db5487130d54ea7af9
Stephan Plepelits <skunk@xover.mud.at>
2012-06-06 06:22:30 +0200

    Howto include modules

--- test.php
+++ test.php
@@ -16,5 +16,12 @@ This page demonstrates the use of the framework. Clone the submodules into the i
     </ul>
   </ul>
 </ul>
+<p>
+Commands to use to clone submodules:<br>
+<pre>
+git submodule add git://github.com/plepe/Form.git inc/form
+git add .gitmodules inc/form
+git commit
+</pre>
 </body>
 </html>
