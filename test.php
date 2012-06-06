8ed24e2de7589129dc35b580d29df4d31e41a29a
Stephan Plepelits <skunk@xover.mud.at>
2012-06-06 06:17:58 +0200

    Initial commit

---
+++ test.php
@@ -0,0 +1,20 @@
+<?
+include "inc/loader.php";
+?>
+<html>
+<head>
+  <title>Framework Test</title>
+</head>
+<body>
+<p>
+This page demonstrates the use of the framework. Clone the submodules into the inc/-directory, e.g.:
+<ul>
+  <li>/<ul>
+    <li>inc/<ul>
+      <li>form/ ...</li>
+      <li>lang/ ...</li>
+    </ul>
+  </ul>
+</ul>
+</body>
+</html>
