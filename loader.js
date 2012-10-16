02c96847ccf20826ff38c3637b794a54f2927482
Stephan Plepelits <skunk@xover.mud.at>
2012-10-16 15:41:44 +0200

    modulekit_loaded() for JS

--- loader.js
+++ loader.js
@@ -1,3 +1,18 @@
+function modulekit_loaded(module) {
+  if((!modulekit)||
+     (!modulekit.aliases))
+    return false;
+
+  if(modulekit.aliases[module])
+    module=modulekit.aliases[module];
+
+  for(var i=0; i<modulekit.load.length; i++)
+    if(modulekit.load[i]==module)
+      return true;
+
+  return false;
+}
+
 function modulekit_file(module, path) {
   if(!path)
     path="";
