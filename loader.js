432f508df5b04de369e4d98cf7f3c65b74ebea15
Stephan Plepelits <skunk@xover.mud.at>
2012-10-16 21:24:57 +0200

    modulekit_get_includes: define in JS, documentation

--- loader.js
+++ loader.js
@@ -25,3 +25,22 @@ function modulekit_file(module, path) {
 
   return modulekit.modules[module].path+"/"+path;
 }
+
+function modulekit_get_includes(type) {
+  var list=[];
+
+  if((!modulekit)||
+     (!modulekit.modules)||
+     (!modulekit.order))
+    return [];
+
+  for(var i=0; i<modulekit.order.length; i++) {
+    var m=modulekit.order[i];
+
+    if(modulekit.modules[m].include&&modulekit.modules[m].include[type])
+      for(var j=0; j<modulekit.modules[m].include[type].length; j++)
+	list.push(modulekit_file(m, modulekit.modules[m].include[type][j]));
+  }
+
+  return list;
+}
