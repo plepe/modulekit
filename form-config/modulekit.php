3a58f5e646695cdfa22b04be7555b6b3e965ca26
Stephan Bösch-Plepelits <skunk@cg.tuwien.ac.at>
2015-06-24 09:56:43 +0200

    Form Config: move to base directory; rename & add description

---
+++ form-config/modulekit.php
@@ -0,0 +1,13 @@
+<?php
+$id="modulekit-form-config";
+
+$desc="A form for configuring modulekit modules. Does not need any further dependencies.";
+
+$include=array(
+  'php'=>array(
+    "code.php",
+  ),
+  'css'=>array(
+    "style.css",
+  ),
+);
